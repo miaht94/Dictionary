3064eaa7f8ecf6b181e86960ec07b7c2e74fe40a
Nguyễn Việt Anh <61254534+vakoyomi@users.noreply.github.com>
2020-09-23 15:53:35 +0700

    created fork project + make simple UI
    window size: 1000x768

--- src/main/java/dictionary/Main.java
+++ src/main/java/dictionary/Main.java
@@ -8,9 +8,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/certainUI.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
