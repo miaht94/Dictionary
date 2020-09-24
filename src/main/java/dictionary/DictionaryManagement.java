121f6ae75f3d145d3cff3dcd5f760edb8ceda0cb
TX_Bach <tranxuanbach1@gmail.com>
2020-09-24 20:35:30 +0700

    Add read from file method
    Read from file just only read from dictionary.txt in resources directory

--- src/main/java/dictionary/DictionaryManagement.java
+++ src/main/java/dictionary/DictionaryManagement.java
@@ -1,13 +1,38 @@
 package dictionary;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class DictionaryManagement {
-    public static Scanner sc;
+
     public static void insertFromCommandline() {
-        sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
         sc.nextLine();
         IntStream.range(0, length).forEach(i -> Dictionary.getInstance().addWord(new Word(sc.nextLine(),sc.nextLine())));
+    }
+
+    public static void insertFromFile() {
+        String path = DictionaryManagement.class.getClassLoader().getResource("dictionary.txt").getPath().substring(1);
+        File file = new File(path);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+                String a = sc.nextLine();
+                String[] b = a.split("\t");
+                Dictionary.getInstance().addWord(new Word(b[0], b[1]));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Khong tim thay file dictionary");
+        }
+
+    }
+
+    // Testing
+    public static void main(String[] args) {
+        insertFromFile();
+        DictionaryCommandline.showAllWords();
     }
 }
