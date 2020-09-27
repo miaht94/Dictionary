12056d78ff09a15622d8a5a6ed47b1a505592738
Trần Xuân Bách <tranxuanbach1@gmail.com>
2020-09-27 19:21:31 +0700

    Fix Text List doesn't the alphabet order
    +) Add compareTo to supply for binarySearch in the future
    +) Add function get Comparator to sort Text List (Example : when search Eat, word eat will appear first

--- src/main/java/Model/Word.java
+++ src/main/java/Model/Word.java
@@ -1,9 +1,8 @@
 package Model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
-public class Word {
+public class Word implements Comparable {
     private String id;
     private String title;
     private String XML;
@@ -36,5 +35,24 @@ public class Word {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        String target = ((Word)o).getTitle();
+        if (target.length() > this.getTitle().length())
+            target = target.substring(0, this.getTitle().length());
+        return this.getTitle().compareTo(target);
+    }
+
+    public static Comparator<Word> getStandardComparator() {
+        return new Comparator<Word>() {
+            @Override
+            public int compare(Word o1, Word o2) {
+                String os1 = o1.getTitle().toLowerCase();
+                String os2 = o2.getTitle().toLowerCase();
+                return os1.compareTo(os2);
+            }
+        };
     }
 }
